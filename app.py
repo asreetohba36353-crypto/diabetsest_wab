08f63c3abb5c6c1a1666f6f2d232f9e882c44d6a
asreetohba36353-crypto <asreetohba36353@gmail.com>
2025-11-15 03:37:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 import numpy as np
 
-model = joblib.load("Diabetes.pkl")
+model = joblib.load('Diabetes.pkl')
 
 st.title("🔍 Diabetes Prediction Web App")
 
@@ -23,4 +23,5 @@ if st.button("Predict"):
         st.error(f"ผลทำนาย: เสี่ยงเป็นเบาหวาน (โอกาส {prob*100:.2f}%)")
     else:
         st.success(f"ผลทำนาย: ไม่เสี่ยง (โอกาส {prob*100:.2f}%)")
+
 
